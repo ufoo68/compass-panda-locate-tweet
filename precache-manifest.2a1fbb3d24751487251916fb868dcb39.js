f51f0d1d8f4e154df5b8f577a2fdd021d238debc
ufoo68 <mitarou.yuutarou@gmail.com>
2019-07-29 22:53:46 +0900

    Updates

---
+++ precache-manifest.2a1fbb3d24751487251916fb868dcb39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d48a6b4f0ae2229c20ef79bbc66e3e",
+    "url": "/compass-panda-locate-tweet/index.html"
+  },
+  {
+    "revision": "ca725f665d06d43af97a",
+    "url": "/compass-panda-locate-tweet/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/compass-panda-locate-tweet/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ca725f665d06d43af97a",
+    "url": "/compass-panda-locate-tweet/static/js/main.49bce3bd.chunk.js"
+  },
+  {
+    "revision": "43321f2698932a6bddee",
+    "url": "/compass-panda-locate-tweet/static/js/runtime~main.53d09f93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/compass-panda-locate-tweet/static/media/logo.5d5d9eef.svg"
+  }
+]);
