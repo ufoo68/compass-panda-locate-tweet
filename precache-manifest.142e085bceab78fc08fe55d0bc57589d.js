7efb5e4c6d22e42aec8a5e380afc71de0ccd4bca
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2019-07-30 12:40:02 +0900

    Updates

---
+++ precache-manifest.142e085bceab78fc08fe55d0bc57589d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a384780375677e1b61f954d69089a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2fd2ae7979dfab7b839",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2977ebbde0adebd4aaf7",
+    "url": "/static/js/2.34c58dd9.chunk.js"
+  },
+  {
+    "revision": "b2fd2ae7979dfab7b839",
+    "url": "/static/js/main.41bb30e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/compass-panda-locate-tweet/precache-manifest.2a1fbb3d24751487251916fb868dcb39.js"
+  "/precache-manifest.142e085bceab78fc08fe55d0bc57589d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/compass-panda-locate-tweet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
